115387b6bb9bf954c77c734b91aa2b3f78597912
Rajeshkumar Elumalai <38979122+elumalair@users.noreply.github.com>
2020-01-08 21:29:35 +0530

    Create Concatenation6.java

---
+++ Concatenation6.java
@@ -0,0 +1,19 @@
+public String convertToUpperCase( final String name) {
+    return name.toUpperCase();
+}
+ 
+public String convertToLowerCase( final String name) {
+    return name.toLowerCase();
+}
+ 
+public String trimSpaces( final String name) {
+    return name.trim();
+}
+ 
+public int calculateLength( final String name) {
+    return name.length();
+}
+ 
+public boolean isEmpty( final String name) {
+    return name.isEmpty();
+}
