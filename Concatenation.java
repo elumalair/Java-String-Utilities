48bc22c2796014fccef76d59fc73c9768fbed210
Rajeshkumar Elumalai <38979122+elumalair@users.noreply.github.com>
2020-01-08 21:24:35 +0530

    Create Concatenation.java

---
+++ Concatenation.java
@@ -0,0 +1,18 @@
+public class Concatenation {
+ 
+    public static void main(String[] args) {
+ 
+        //input
+        String firstName = "Praveen" ;
+        String lastName = "Kumar";
+         
+        //business logic
+        String fullName = firstName + lastName;     
+         
+        //output
+        System.out.println(fullName);
+                 
+ 
+    }
+ 
+}
